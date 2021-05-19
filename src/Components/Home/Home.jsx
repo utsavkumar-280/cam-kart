456c14fdeb9e29dc88fc9bc4cc16de695d917e8e
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-19 16:20:09 +0530

    fix: fixed the redirecting flickering

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,19 @@
 import "./home.css";
+// import Hero from "./assets/heroBanner.png";
 
 export const Home = () => {
   return (
     <div className="home-container">
-      <div className="home-main">
-       <h2>Hello its me Homepage</h2>
-      </div>
+        <section className="hero">
+          <h2>Hello its me Homepage</h2>
+      </section>
+      <section className="catagory-container">
+
+        </section>
+        <section className="brand-container">
+
+        </section>
+
       
     </div>
   )
