4672ddff3fded0a8b19e8e481ac1abe50e7fdd85
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-22 08:47:08 +0530

    feat: added some more categories

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import Dslr from "./assets/dslr.png";
 import Mirrorless from "./assets/mirrorless.png";
 import Action from "./assets/action.png";
+import Instant from "./assets/instant.png"
 import Lens from "./assets/lens.png";
 import Gimbal from "./assets/gimbal.png";
+import Tripod from "./assets/tripod.png"
 import Accessories from "./assets/accessories.png";
 
 export const Home = () => {
@@ -34,18 +36,23 @@ export const Home = () => {
           <div className="home-head-text">Category</div>
           <div className="cat-main-grid">
             <div className="cat-card-container">
-              <div className="cat-card-head">DSLR</div>
+              <div className="cat-card-head">DSLR CAMERAS</div>
               <img src={Dslr} alt="dslr-img" className="cat-card-img" />
               <Link to="/products"><button className="cat-card-button">Shop Now</button></Link>
             </div>
             <div className="cat-card-container">
-              <div className="cat-card-head">MIRRORLESS</div>
+              <div className="cat-card-head">MIRRORLESS CAMERAS</div>
               <img src={ Mirrorless} alt="dslr-img" className="cat-card-img" />
               <Link to="/products"><button className="cat-card-button">Shop Now</button></Link>
             </div>
             <div className="cat-card-container">
-              <div className="cat-card-head">ACTION</div>
+              <div className="cat-card-head">ACTION CAMERAS</div>
               <img src={ Action} alt="dslr-img" className="cat-card-img" />
+              <Link to="/products"><button className="cat-card-button">Shop Now</button></Link>
+            </div>
+            <div className="cat-card-container">
+              <div className="cat-card-head">INSTANT CAMERAS</div>
+              <img src={ Instant} alt="dslr-img" className="cat-card-img" />
               <Link to="/products"><button className="cat-card-button">Shop Now</button></Link>
             </div>
             <div className="cat-card-container">
@@ -54,8 +61,13 @@ export const Home = () => {
               <Link to="/products"><button className="cat-card-button">Shop Now</button></Link>
             </div>
             <div className="cat-card-container">
-              <div className="cat-card-head">GIMBAL</div>
+              <div className="cat-card-head">GIMBALS</div>
               <img src={ Gimbal} alt="dslr-img" className="cat-card-img" />
+              <Link to="/products"><button className="cat-card-button">Shop Now</button></Link>
+            </div>
+            <div className="cat-card-container">
+              <div className="cat-card-head">TRIPODS</div>
+              <img src={ Tripod} alt="dslr-img" className="cat-card-img" />
               <Link to="/products"><button className="cat-card-button">Shop Now</button></Link>
             </div>
             <div className="cat-card-container">
@@ -68,14 +80,46 @@ export const Home = () => {
         <section className="brand-container">
           <div className="home-head-text">Shop by Brand</div>
           <div className="brand-main-grid">
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand-sony brand"></div></div></Link>
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand brand-canon"></div></div></Link>         
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand brand-nikon"></div></div></Link>
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand brand-panasonic"></div></div></Link>
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand brand-fujifilm"></div></div></Link>
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand brand-sigma"></div></div></Link>
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand brand-dji"></div></div></Link>
-            <Link to="/wishlist"><div className="brand-logo"><div className=" brand brand-gopro"></div></div></Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand-sony brand"></div>
+              </div>
+            </Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand brand-canon"></div>
+              </div>
+            </Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand brand-nikon"></div>
+              </div>
+            </Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand brand-panasonic"></div>
+              </div>
+            </Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand brand-fujifilm"></div>
+              </div>
+            </Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand brand-sigma"></div>
+              </div>
+            </Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand brand-dji"></div>
+              </div>
+            </Link>
+            <Link to="/wishlist">
+              <div className="brand-logo">
+                <div className=" brand brand-gopro"></div>
+              </div>
+            </Link>
           </div>
         </section>
       </div>
