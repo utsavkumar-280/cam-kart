92a2a8b208e99bb32e3630e457cea56bd6800ff7
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-31 13:43:28 +0530

    refact: refactored classname

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,29 +1,40 @@
 import "./footer.css";
 import { Link } from "react-router-dom";
 
-export const Footer=()=> {
-  return (
-    <div className="footer-container">
-      <div className="footer-main">
-        <div className="footer-left ">
-          <div className="footer-padding footer-para">Connect with me</div>
-          <div className="socialNav">
-            <a href="https://www.linkedin.com/in/utsav-kumar-a3a79b187/"><i class="fab fa-linkedin-in"></i></a>
-          <a href="https://twitter.com/utsavkumar280"><i class="fab fa-twitter"></i></a>
-          <a href="https://github.com/utsavkumar-280"><i class="fab fa-github"></i></a>
-          </div>
-          
-        </div>
+export const Footer = () => {
+	return (
+		<div className="footer-container">
+			<div className="footer-main">
+				<div className="footer-left ">
+					<div className="footer-padding footer-para">Connect with me</div>
+					<div className="socialNav">
+						<a href="https://www.linkedin.com/in/utsav-kumar-a3a79b187/">
+							<i className="fab fa-linkedin-in"></i>
+						</a>
+						<a href="https://twitter.com/utsavkumar280">
+							<i className="fab fa-twitter"></i>
+						</a>
+						<a href="https://github.com/utsavkumar-280">
+							<i className="fab fa-github"></i>
+						</a>
+					</div>
+				</div>
 
-
-        <div className="footer-right">
-          <Link to="/" className="footer-logo">CamKart</Link>
-        <div className="connectMe">
-          <div className="footer-name-padding">BY</div>
-          <a href="https://utsav-kumar.netlify.app/" className=" footer-name-padding">@UTSAV</a>
-        </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+				<div className="footer-right">
+					<Link to="/" className="footer-logo">
+						CamKart
+					</Link>
+					<div className="connectMe">
+						<div className="footer-name-padding">BY</div>
+						<a
+							href="https://utsav-kumar.netlify.app/"
+							className=" footer-name-padding"
+						>
+							@UTSAV
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
