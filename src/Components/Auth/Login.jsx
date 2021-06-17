9a9f359000d3e9f27810aa9bf82e2d263d14c7dc
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-17 19:15:54 +0530

    feat: added chakra forms

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,15 +1,62 @@
 import "./authStyles.css";
-// import { Footer } from "../Footer";
+import {
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	Input,
+	Button,
+} from "@chakra-ui/react";
+
+import { Formik, Field, Form } from "formik";
 
 export const Login = () => {
-  return (
-    <div className="login">
-      <div className="login-container">
-        <div className="login-main"></div>
-          <h2>Hello its me login</h2>
-      </div>
-      
-    </div>
-    
-  )
-}
+	function validateName(value) {
+		let error;
+		if (!value) {
+			error = "Name is required";
+		} else if (value.toLowerCase() !== "naruto") {
+			error = "Jeez! You're not a fan 😱";
+		}
+		return error;
+	}
+	return (
+		<div className="login">
+			<div className="login-container">
+				<div className="login-main"></div>
+				<Formik
+					initialValues={{ name: "Sasuke" }}
+					onSubmit={(values, actions) => {
+						setTimeout(() => {
+							alert(JSON.stringify(values, null, 2));
+							actions.setSubmitting(false);
+						}, 1000);
+					}}
+				>
+					{(props) => (
+						<Form>
+							<Field name="name" validate={validateName}>
+								{({ field, form }) => (
+									<FormControl
+										isInvalid={form.errors.name && form.touched.name}
+									>
+										<FormLabel htmlFor="name">First name</FormLabel>
+										<Input {...field} id="name" placeholder="name" />
+										<FormErrorMessage>{form.errors.name}</FormErrorMessage>
+									</FormControl>
+								)}
+							</Field>
+							<Button
+								mt={4}
+								colorScheme="teal"
+								isLoading={props.isSubmitting}
+								type="submit"
+							>
+								Submit
+							</Button>
+						</Form>
+					)}
+				</Formik>
+			</div>
+		</div>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppDataProvider } from "./Context";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<AppDataProvider>
-			<Router>
-				<App />
-			</Router>
-		</AppDataProvider>
+		<Router>
+			<ChakraProvider>
+				<AppDataProvider>
+					<App />
+				</AppDataProvider>
+			</ChakraProvider>
+		</Router>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
