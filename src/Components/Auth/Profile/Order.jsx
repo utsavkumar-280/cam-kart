326ada2754047b5ed219f5cb73ac59dfe2d7b873
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2021-08-03 19:46:05 +0530

    feat: initialized orders page

--- src/Components/Auth/Profile/Order.jsx
+++ src/Components/Auth/Profile/Order.jsx
@@ -1,3 +1,23 @@
+import { useAppDataContext } from "../../../Context";
+
 export const Order = () => {
-	return <div>This is a Order</div>;
+	const {
+		state: { orders },
+	} = useAppDataContext();
+	return (
+		<div className="orders-page-container">
+			<div className="orders-page-content">
+				<h1>Orders</h1>
+				{orders?.length === 0 ? (
+					<div>No orders yet</div>
+				) : (
+					orders?.map((order) => (
+						<div key={order._id}>
+							<div>Order id:{order._id} </div>
+						</div>
+					))
+				)}
+			</div>
+		</div>
+	);
 };
